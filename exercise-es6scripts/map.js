84778e2de7d5cb934ec05436c4f35bb16d91c6b2
mbedada04 <mbedada@atl.yearup.org>
2022-10-18 10:27:09 -0400

    array_method

---
+++ exercise-es6scripts/map.js
@@ -0,0 +1,15 @@
+let kids = [
+{ first : "Natalie", last : "Plyers" },
+ { first: "Brittany", last: "Ray" },
+ { first: "Zachary", last: "Westly" }
+];
+
+
+// let kidsNames =[];
+ for (let index = 0; index < kids.length; index++) {
+    const element = kids[index];
+    kidsNames.push(`${kid.first} ${kid.last}`)
+    
+ }
+ console.log(kidsNames);
+ 
